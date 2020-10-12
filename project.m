e6e51073ea16721be13fb1ccaa89350fe7974d97
Jonathan F. Roth <jfr9112@rit.edu>
2020-10-12 01:22:40 -0400

    added images, main matlab file, .gitignore

---
+++ project.m
@@ -0,0 +1,16 @@
+%   Parker Johnson
+%   Jonathan Roth
+%   2020-08-11
+
+function project(input_path, output_path)
+
+%disp(input_path);
+%disp(output_path);
+
+im = im2double(imread(input_path));
+imshow(im);
+
+
+
+
+    
